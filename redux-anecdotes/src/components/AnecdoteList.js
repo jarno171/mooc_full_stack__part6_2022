88b69035d3efa2be5da6745ebc024fe4055edca0
Jarno Varis <jarno.varis@gmail.com>
2022-11-15 18:14:36 +0200

    added notification for showing who you voted for, with 5 sec timer

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,11 +1,22 @@
+import { useRef } from 'react'
 import { incrementVote, } from '../reducers/anecdoteReducer'
+import { setNotification, resetNotification } from '../reducers/notificationReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
-const vote = (dispatcher, id) => {
+const vote = (dispatcher, id, content, clearTimerRef) => {
+  clearTimeout(clearTimerRef.current)
+
   dispatcher(incrementVote(id))
+  dispatcher(setNotification(`you voted for "${content}"`))
+
+  clearTimerRef.current = setTimeout(() => {
+    dispatcher(resetNotification())
+  }, 5000)
 }
 
 const AnecdoteList = () => {
+
+  const clearTimerRef = useRef();
 
   const anecdotes = useSelector(state => state.anecdotes)
   const dispatch = useDispatch()
@@ -20,7 +31,7 @@ const AnecdoteList = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(dispatch, anecdote.id)}>vote</button>
+            <button onClick={() => vote(dispatch, anecdote.id, anecdote.content, clearTimerRef)}>vote</button>
           </div>
         </div>
       )}
