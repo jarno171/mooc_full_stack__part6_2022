a6575e35f938f159e0df5646ec45c8a2c9db3431
Jarno Varis <jarno.varis@gmail.com>
2022-11-16 19:30:33 +0200

    initializing the store now works with Redux Thunk

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const sortAnecdotes = (anecdotes) => {
   return anecdotes.sort((a,b) => (a.votes > b.votes) ? -1 : ((b.votes > a.votes) ? 1 : 0))
@@ -28,3 +29,10 @@ const anecdoteSlice = createSlice({
 
 export const { incrementVote, addAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
